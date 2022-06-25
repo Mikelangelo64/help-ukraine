64811b78751fa6d2302323a49f2ca38679fb7079
Mikelangelo64 <donni2001331gmail.com>
2022-06-25 21:49:42 +0300

    v-1.3

--- js/script.js
+++ js/script.js
@@ -6,4 +6,34 @@ $(document).ready(function(){
         $('body').toggleClass('_lock')
 
     })
+
+    //SORT-active
+    $('.sort').click(function(e){
+        $(this).toggleClass('_active-sort')
+        $(".sort__choise").toggleClass('_active-sort')
+    })
+
+    //swiper`s
+
+    //swiper ruls
+    let rulsCategories = ['Постраждалі', 'Будівельні компанії', 'Юридичні компанії', 'Ангели']
+
+    let rulsSwiper = new Swiper('.ruls-swiper.swiper', {
+        slidesPerView: 1,
+
+        allowTouchMove: false,
+
+        spaceBetween: 80,
+        autoHeight: true,
+        loop: false,
+        pagination: {
+            
+            el: '.swiper-pagination.ruls-list__title__container',
+            clickable: true,
+            renderBullet: function (index, className) {
+                return `<span class="ruls-list__title ${className}">${rulsCategories[index]}</span>`;
+            },
+        },
+        
+    })
 })
